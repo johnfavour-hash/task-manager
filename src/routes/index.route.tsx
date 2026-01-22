e76c6fd24b2f7bf8e55b5e0a82e08b35d1f34c79
Friday Joshua <friday.joshua@speedlinkng.com>
2026-01-22 13:19:28 +0100

    feat: Initialize React project with Vite, comprehensive authentication, API integration, and core UI components.

---
+++ src/routes/index.route.tsx
@@ -0,0 +1,33 @@
+import { Route, Routes, BrowserRouter } from "react-router";
+import AuthRoutes from "./auth.route";
+import ProfileRoutes from "./profile.route";
+import { lazy } from "react";
+import RootLayout from "@app/layouts/layout";
+
+const DashboardPage = lazy(() => import("../app/pages/dashboard/page"));
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* public routes */}
+                <Route element={<RootLayout />}>
+                    <Route path="/" element={<DashboardPage />} />
+                    <Route path="/about" element={<p>About</p>} />
+                    <Route path="/contact" element={<p>Login</p>} />
+                </Route>
+            </Routes>
+
+
+
+            {/* auth routes */}
+            <AuthRoutes />
+
+            {/* protected routes */}
+            <ProfileRoutes />
+        </BrowserRouter >
+    )
+}
+
+export default Router;
